b58be2793d28ac89e525d5c183d27ee254dcde9e
Megan :) <imafool68@gmail.com>
2021-04-13 16:48:01 -0700

    Completed FoodFrenzy Unity project

---
+++ MLFoodFrenzy/Assets/Scripts/HUD.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class HUD : MonoBehaviour
+{
+    public Level level;
+    public GameOver gameOver;
+
+    public Text remainingText;
+    public Text remainingSubtext;
+    public Text targetText;
+    public Text targetSubtext;
+    public Text scoreText;
+
+    public Image[] stars;
+
+    private int starIndex;
+    private bool isGameOver;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        UpdateStars();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void UpdateStars()
+    {
+        for (int i = 0; i < stars.Length; i++)
+        {
+            if (i == starIndex)
+            {
+                stars[i].enabled = true;
+            }
+            else
+            {
+                stars[i].enabled = false;
+            }
+        }
+    }
+
+    public void SetScore(int score)
+    {
+        scoreText.text = score.ToString();
+
+        int visibleStar = 0;
+
+        if (score >= level.score1Star && score < level.score2Star)
+        {
+            visibleStar = 1;
+        }
+        else if (score >= level.score2Star && score < level.score3Star)
+        {
+            visibleStar = 2;
+        }
+        else if (score >= level.score3Star)
+        {
+            visibleStar = 3;
+        }
+
+        starIndex = visibleStar;
+        UpdateStars();
+    }
+
+    public void SetTarget(int target)
+    {
+        targetText.text = target.ToString();
+    }
+
+    public void SetRemaining(int remaining)
+    {
+        remainingText.text = remaining.ToString();
+    }
+
+    public void SetRemaining(string remaining)
+    {
+        remainingText.text = remaining;
+    }
+
+    public void SetLevelType(Level.LevelType type)
+    {
+        switch (type)
+        {
+            case Level.LevelType.MOVES:
+                remainingSubtext.text = "Moves Remaining";
+                targetSubtext.text = "Target Score";
+                break;
+            case Level.LevelType.OBSTACLE:
+                remainingSubtext.text = "Moves Remaining";
+                targetSubtext.text = "Dishes Remaining";
+                break;
+            case Level.LevelType.TIMER:
+                remainingSubtext.text = "Time Remaining";
+                targetSubtext.text = "Target Score";
+                break;
+        }
+    }
+
+    public void OnGameWin(int score)
+    {
+        gameOver.ShowWin(score, starIndex);
+        if(starIndex > PlayerPrefs.GetInt(SceneManager.GetActiveScene().name, 0))
+        {
+            PlayerPrefs.SetInt(SceneManager.GetActiveScene().name, starIndex);
+        }
+    }
+
+    public void OnGameLose()
+    {
+        gameOver.ShowLose();
+    }
+}
---
+++ MLFoodFrenzy/Assets/Scripts/LevelSelect.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour
+{
+    [System.Serializable]
+    public struct ButtonPlayerPrefs
+    {
+        public GameObject gameObject;
+        public string playerPrefKey;
+    }
+
+    public ButtonPlayerPrefs[] buttons;
+
+    private void Start()
+    {
+        for(int i = 0; i < buttons.Length; i++)
+        {
+            int score = PlayerPrefs.GetInt(buttons[i].playerPrefKey, 0);
+
+            for (int starIndex = 1; starIndex <= 3; starIndex++)
+            {
+                Transform star = buttons[i].gameObject.transform.Find("star" + starIndex);
+                if(starIndex <= score)
+                {
+                    star.gameObject.SetActive(true);
+                }
+                else
+                {
+                    star.gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+
+
+    public void OnButtonPress(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+}
